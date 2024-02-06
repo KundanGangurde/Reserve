f6875369d877162cf023349bc56443c2eb4af770
KundanGangurde <gangordaykondan@gmail.com>
2024-02-06 10:29:36 +0530

    first

--- server/server.js
+++ server/server.js
@@ -6,18 +6,19 @@ const app = express();
 // app.use(cors());
 app.use(express.json());
 
-mongoose.connect(
-    "mongodb+srv://gangordaykondan:gangordaykondan@cluster0.s34wiae.mongodb.net/db?retryWrites=true&w=majority"
-    ).then(()=>{
-        console.log("mongoose is connected")
-    }).catch((err)=>{
-        console.log({err})
-    })
+mongoose
+  .connect
+  // "mongodb+srv://gangordaykondan:gangordaykondan@cluster0.s34wiae.mongodb.net/db?retryWrites=true&w=majority"
+  ()
+  .then(() => {
+    console.log("mongoose is connected");
+  })
+  .catch((err) => {
+    console.log({ err });
+  });
 
-    app.get('/gettrips', (req,res)=>{
-        
-    })
+app.get("/gettrips", (req, res) => {});
 
-app.listen(3001, ()=> {
-    console.log("server is running")
-})
+app.listen(3001, () => {
+  console.log("server is running");
+});
